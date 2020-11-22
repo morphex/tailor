2f76c0ae5a91052c8aae532e33708c06783cb69d
Morten W. Petersen <morphex@gmail.com>
2020-11-22 17:44:11 +0100

    Initial commit

---
+++ tailor.py
@@ -0,0 +1,6 @@
+__doc__ = """Small module to process data and take appropriate action
+	from programs that produce output on a line-by-line basis."""
+
+def tailor(command, actions, capture_stdin=True, capture_stdout=True)
+    pass
+
