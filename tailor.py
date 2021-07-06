15d888dc04f6a048f78efef282aa1e1fee01efa5
Morten W. Petersen <morphex@gmail.com>
2021-07-06 15:40:24 +0200

    Have working 'tail -f' equivalent

--- tailor.py
+++ tailor.py
@@ -1,33 +1,94 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.7
 
 __doc__ = """Small module to process data and take appropriate action
 	from programs that produce output on a line-by-line basis."""
 
-import subprocess, sys, time
+import subprocess, sys, time, re, tempfile, os, shlex
 
-def tail_command(command, shell=True):
-    result = subprocess.run(command, capture_output=True, shell=shell)
-    for line in result.stdout.decode().split('\n'):
-        yield line
+ansi_re = re.compile("\x1b\[[0-9;]*[mGKHF]")
+
+def filter_ansi(string_):
+    return ansi_re.sub("", string_)
+
+def tail_command_old(command):
+    stdout = tempfile.mktemp()
+    stderr = tempfile.mktemp()
+    command = "/bin/sh -c '%s 1> %s 2> %s &'" % (command, stdout, stderr)
+    result = subprocess.run(command, shell=True)
+    stdout_f = open(stdout, 'r')
+    stderr_f = open(stderr, 'r')
+    while True:
+        line = stdout_f.readline()
+        if line:
+            yield line
+        else:
+            time.sleep(0.01)
+
+def tail_command(command):
+    command = shlex.split(command)
+    result = subprocess.Popen(command, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
+    count = 0
+    while True:
+        line = result.stdout.readline().decode()
+        if line:
+            yield line
+        else:
+            count += 1
+            if result.returncode == None:
+                if count % 10:
+                    time.sleep(0.01)
+                else:
+                    if os.system("ps -%s > /dev/null " % result.pid):
+                        return
+            else:
+                return
+
+class GotEOF(Exception):
+    "Exception raised when EOF of file is found, by empty readline"
+    pass
 
 def tail(filename):
     return tail_f(filename, follow=False)
 
 def tail_f(filename, follow=True):
     file = open(filename, 'r')
+    try:
+        for line in _tail_f(file, follow=follow, signal_wait=True):
+            yield line
+    except GotEOF:
+        return _tail_f(file, follow=follow)
+
+def _tail_f(file, follow=True, signal_wait=False):
     line = ''
     while True:
         line = file.readline()
         if line:
             yield line
         else:
-            if not follow: break
+            if not follow: return
+            if signal_wait:
+                raise GotEOF()
             time.sleep(0.1)
+
+def get_tail_of_tail_f(filename, follow=True, tail_size=10):
+    file = open(filename, 'r')
+    lines = []
+    try:
+        for line in _tail_f(file, follow=follow, signal_wait=True):
+            lines.append(line)
+    except GotEOF:
+        for line in lines[len(lines) - tail_size:]:
+            yield line
+    for line in _tail_f(file, follow=follow):
+        yield line
 
 if __name__ == '__main__':
     #for line in tail_command("cat %s" % sys.argv[0]):
     #    print(line)
-    for line in tail(sys.argv[0]):
+    for line in tail_command("ls --color -l"):
+        print(filter_ansi(line))
+    for line in tail_command("./output_ansi.py"):
+        print(filter_ansi(line))
+    for line in get_tail_of_tail_f("/var/log/syslog"):
         print(line, end='')
-    #for line in tail_f("/var/log/syslog"):
-    #    print(line, end='')
